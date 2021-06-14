ec3a810617896a72d9e056ca5f8dc65574023e31
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-14 16:51:34 +0100

    Initial commit

---
+++ tests/Tutorial.cpp
@@ -0,0 +1,7 @@
+#define APPROVAL_TESTS_HIDE_DEPRECATED_CODE 1
+
+#include "ApprovalTests.hpp"
+#include "catch2/catch.hpp"
+
+// See the tutorial at:
+//   https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/Tutorial.md#top
